761534d6e0b89accefc46d68ab4832604b40fa5f
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-05-01 14:27:11 +0900

    [level 2] Title: 모음 사전, Time: 39.48 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,40 +1,31 @@
-import java.util.*;
-
 class Solution {
     
-    static char[] strArray = {'A', 'E', 'I', 'O', 'U'};
-    static char[] selected;
-    static List<String> list = new ArrayList<>();
-    static StringBuilder sb = new StringBuilder();
+    static char[] charArray = {'A', 'E', 'I', 'O', 'U'};
+    static int count = 0;
+    static int answer = 0;
     
     public int solution(String word) {
         
-        for(int j=1; j<=strArray.length; j++) {
-            selected = new char[j];
-            backTracking(0, j, word);
-        }
+        dfs("", word);
         
-        Collections.sort(list);
-        
-        return list.indexOf(word) + 1;
+        return answer;
     }
     
-    public void backTracking(int pick, int count, String word) {
-        if (pick == count) {
-            sb.setLength(0);
-            
-            for(char c : selected) {
-                sb.append(c);
-            }
-            
-            list.add(sb.toString());
-            
+    public void dfs(String str, String word) {
+        
+        if (str.length() > 5) {
             return;
         }
         
-        for(int i=0; i<strArray.length; i++) {
-            selected[pick] = strArray[i];
-            backTracking(pick + 1, count, word);
+        if (word.equals(str)) {
+            answer = count;
+            return;
+        }
+        
+        count++;
+        
+        for(char c : charArray) {
+            dfs(str + c, word);
         }
     }
 }
