79a8686967f8cf08157755fa7cf1d41bbbcc00ca
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-05-01 21:06:04 +0900

    [level 2] Title: 소수 찾기, Time: 107.07 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -6,50 +6,47 @@ class Solution {
     static Set<Integer> set = new HashSet<>();
     
     public int solution(String numbers) {
-       
+        int answer = 0;
         
-        for(int i=1; i<=numbers.length(); i++) {
-            visited = new boolean[numbers.length()];
-            dfs(0, i, "", numbers.split(""));
-        }
-            
-        return set.size();
-    }
-    
-    static void dfs(int count, int length, String str, String[] numbers) {
+        visited = new boolean[numbers.length()];
+        dfs("", numbers);
         
-        if (count == length) {
-            int num = Integer.parseInt(str);
-            isOdd(num);
-            return;
-        }
-        
-        for(int i=0; i<numbers.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(count + 1, length, str + numbers[i], numbers);
-                visited[i] = false;
+        for(int n : set) {
+            if (!check(n)) {
+                answer++;
             }
         }
         
+        return answer;
     }
     
-    static void isOdd(int num) {
-        
-        boolean even = false;
-                
-        if (num <= 2) {
-            if (num != 2)
-                even = true;
-        } else {
-            for(int i=2; i < num; i++) 
-                if(num % i == 0) {
-                    even = true;
-                    break;
-                }
+    public boolean check(int num) {
+        if (num < 2) {
+            return true;
         }
-            
-        if (even ==  false && num > 1) 
-            set.add(num);
+        
+        for(int i=2; i<num; i++) {
+            if (num % i == 0) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public void dfs(String str, String numbers) {
+        
+        if (str.length() > numbers.length()) {
+            return;
+        }
+        
+        for(int i=0; i<numbers.length(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                set.add(Integer.parseInt(str + numbers.charAt(i)));
+                dfs(str + numbers.charAt(i), numbers);
+                visited[i] = false;
+            }
+        }
     }
 }
