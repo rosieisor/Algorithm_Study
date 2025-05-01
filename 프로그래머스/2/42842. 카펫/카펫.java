3eacbaf3c290f5dc8019f4827dc89cf26741b033
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-05-01 16:53:41 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,22 +1,6 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        
-        int[] answer = new int[2];
-        
-        for(int i=1; i<=yellow; i++) {
-            
-            if (yellow % i == 0) {
-                int w = Math.max(i, yellow/i);
-                int h = Math.min(i, yellow/i);
-                
-                if ((w+2) * (h+2) - yellow == brown) {
-                    answer[0] = w + 2;
-                    answer[1] = h + 2;
-                    break;
-                }
-            }
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
