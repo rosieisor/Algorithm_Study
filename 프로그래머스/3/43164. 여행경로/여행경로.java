3cb2792d63e01991c58a32bbead0ecb6dbac7794
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-05-03 15:57:07 +0900

    [level 3] Title: 여행경로, Time: 116.63 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,37 +1,69 @@
 import java.util.*;
 
-class Solution {
+class Airport {
+    String end;
+    boolean visited;
+    
+    Airport(String end) {
+        this.end = end;
+        this.visited = false;
+    }
+}
 
-    static List<String> list = new ArrayList<>();
-    static boolean[] visited;
+class Solution {
+    
+    static Map<String, List<Airport>> map = new HashMap<>();
+    static List<String> routes;
     
     public String[] solution(String[][] tickets) {
-        visited = new boolean[tickets.length];
+        init(tickets);
+        routes.add("ICN");
+        dfs("ICN");
         
-        dfs(0, "ICN", tickets, "ICN ");
-        Collections.sort(list);
         
-        return list.get(0).split(" ");
+        String[] answer = new String[routes.size()];
+        int index = 0;
+        
+        for (String s : routes) {
+            answer[index++] = s;
+        }
+    
+        return answer;
     }
     
-    public void dfs(int count, String start, String[][] tickets, String routes) {
-        if (count == tickets.length) {
-            list.add(routes);
+    public void dfs(String start) {
+        if (map.get(start) == null) {
             return;
         }
         
-        for(int i=0; i<tickets.length; i++) {
-            if (!visited[i] && start.equals(tickets[i][0])) {
-                visited[i] = true;
-                
-                String temp = routes;
-                
-                routes += tickets[i][1] + " ";
-                dfs(count + 1, tickets[i][1], tickets, routes);
-                
-                routes = temp;
-                visited[i] = false;
+        List<Airport> airports = map.get(start);
+        
+        for(Airport a : airports) {
+            if (!a.visited) {
+                a.visited = true;
+                routes.add(a.end);
+                dfs(a.end);
             }
+        }
+    }
+    
+    public void init(String[][] tickets) {
+        map = new HashMap<>();
+        routes = new ArrayList<>();
+        
+        for(int i=0; i<tickets.length; i++) {
+            if (map.get(tickets[i][0]) == null) {
+                List<Airport> list = new ArrayList<>();
+                list.add(new Airport(tickets[i][1]));
+                
+                map.put(tickets[i][0], list);
+            } else {
+                map.get(tickets[i][0]).add(new Airport(tickets[i][1]));
+            }
+        }
+        
+        for(String key : map.keySet()) {
+            Collections.sort(map.get(key), (o1, o2) -> o1.end.compareTo(o2.end));
         }
     }
 }
