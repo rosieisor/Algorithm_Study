fd213911e1567656cef0b79da8f1a8359c5d53d8
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-05-15 20:10:28 +0900

    [level 2] Title: 구명보트, Time: 11.69 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,25 +1,68 @@
 import java.util.*;
 
 class Solution {
+    
+    static List<int[]> list;
+    static int[] reverse;
+    
     public int solution(int[] people, int limit) {
-        Arrays.sort(people);        // [50, 50, 70, 80]
+    
+        reverse = new int[people.length];
         
-        int answer = 0;
-        int left = 0;
-        int right = people.length - 1;
+        reverseOrder(people);
         
-        while(left < right) {
-            if (people[left] + people[right] <= limit) {
-                left++;
-            } 
-            right--;
-            answer++;
+        list = new ArrayList<>();
+        
+        for(int i=0; i<people.length; i++) {
+            list.add(new int[2]);
         }
         
-        if (left == right) {
-            answer++;
+        func(limit);
+        
+        int answer = 0;
+        
+        for(int[] arr : list) {
+            if (arr[0] > 0 && arr[1] >= 0) {
+                answer++;
+            }
         }
         
         return answer;
     }
+    
+    public void func(int limit) {
+        
+        for(int i=0; i<reverse.length; i++) {
+            int[] array;
+            
+            if (reverse[i] > limit / 2) {
+                array = list.get(i);
+                array[0] = reverse[i];
+            } else {
+                for(int j=0; j<list.size(); j++) {
+                    array = list.get(j);
+                    
+                    if (array[1] == 0 && array[0] + reverse[i] <= limit) {
+                        if (array[0] > 0) {
+                            array[1] = reverse[i];
+                            break;
+                        } else {
+                            array[0] = reverse[i];
+                            break;
+                        }
+                    }
+                }
+            }   
+        }
+    }
+    
+    public void reverseOrder(int[] people) {
+        Arrays.sort(people);
+        
+        int index = 0;
+        for(int i=people.length-1; i>= 0; i--) {
+            reverse[index++] = people[i];
+        }
+    }
+    
 }
