bc263601bd3ac64cef9e2987e27a7bb4e46cb5b2
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-08-05 13:38:47 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(br.readLine()).append("??!");
+        
+        System.out.println(sb.toString());
+    }
+}
