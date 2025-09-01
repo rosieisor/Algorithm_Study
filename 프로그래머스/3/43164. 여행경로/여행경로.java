a4b5e25227d9195fc680823dccf31d7ed102592a
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-09-01 22:32:54 +0900

    [level 3] Title: 여행경로, Time: 60.42 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,31 +1,6 @@
 class Solution {
-    boolean[] visited;
-    String result;
-    
     public String[] solution(String[][] tickets) {
-        
-        visited = new boolean[tickets.length];
-        
-        dfs(0, "ICN", "ICN ", tickets);
-        
-        String[] answer = result.split(" ");
+        String[] answer = {};
         return answer;
-    }
-    
-    public void dfs(int pick, String start, String str, String[][] tickets) {
-        if (pick == tickets.length) {
-            if (result == null || result.compareTo(str) > 0) {
-                result = str;
-            } 
-            return;
-        }
-        
-        for(int i=0; i<tickets.length; i++) {
-            if (!visited[i] && tickets[i][0].equals(start)) {
-                visited[i] = true;
-                dfs(pick + 1, tickets[i][1], str + tickets[i][1] +" " , tickets);
-                visited[i] = false;
-            }
-        }
     }
 }
