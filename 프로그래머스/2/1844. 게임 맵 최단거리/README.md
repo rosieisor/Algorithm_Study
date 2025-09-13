ea382605c4422a575783d1c12b35388a631cfbd6
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-09-13 23:33:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.71 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
 
 ### 성능 요약
 
-메모리: 57.6 MB, 시간: 10.48 ms
+메모리: 55.1 MB, 시간: 8.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 02일 02:32:25
+2025년 09월 13일 23:33:53
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,31 +1,21 @@
 import java.util.*;
 
 class Solution {
-    // 오른쪽 아래 왼쪽 위
-    static int[][] move = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    
-    static int[][] count;
-    static boolean[][] visited;
+    int[][] move = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    boolean[][] visited;
     
     public int solution(int[][] maps) {
-        int answer = 0;
-        
-        count = new int[maps.length][maps[0].length];
-        count[0][0] = 1;
-        
         visited = new boolean[maps.length][maps[0].length];
         
-        bfs(0, 0, maps);    
+        bfs(0, 0, maps);
         
-        if (count[maps.length-1][maps[0].length-1] == 0) {
-            return -1;
-        } else {
-            return count[maps.length-1][maps[0].length-1];
-        }
+        return maps[maps.length-1][maps[0].length-1] == 1 ? 
+            -1 : maps[maps.length-1][maps[0].length-1];
     }
     
-    void bfs(int x, int y, int[][] maps) {
+    public void bfs(int x, int y, int[][] maps) {
         Queue<int[]> queue = new LinkedList<>();
+        
         queue.offer(new int[]{x, y});
         visited[x][y] = true;
         
@@ -33,16 +23,20 @@ class Solution {
             int[] current = queue.poll();
             
             for(int i=0; i<move.length; i++) {
-                int dx = current[0] + move[i][0];
-                int dy = current[1] + move[i][1];
+                int mx = current[0] + move[i][0];
+                int my = current[1] + move[i][1];
                 
-                if (dx < maps.length && dy < maps[0].length &&
-                        dx > -1 && dy > -1 && maps[dx][dy] == 1 && !visited[dx][dy]) {
-                    count[dx][dy] = (count[dx][dy] == 0) ? count[current[0]][current[1]] + 1 
-                        : Math.min(count[current[0]][current[1]] + 1, count[dx][dy]);
-                    queue.offer(new int[]{dx, dy});
+                if (mx > -1 && my > -1 && mx < maps.length && my < maps[0].length &&
+                    !visited[mx][my] && maps[mx][my] != 0) {
+                    visited[mx][my] = true;
                     
-                    visited[dx][dy] = true;
+                    if (maps[mx][my] == 1) {
+                        maps[mx][my] += maps[current[0]][current[1]];
+                    } else {
+                        maps[mx][my] = Math.min(maps[mx][my], maps[current[0]][current[1]] + 1);
+                    }
+                    
+                    queue.offer(new int[]{mx, my});
                 }
             }
         }
