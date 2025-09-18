571c851e06e28ed1c6d7e59f66b42893b77a677f
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-09-18 23:11:42 +0900

    [Silver III] Title: N과 M (5), Time: 292 ms, Memory: 71628 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int N;
+    static int M;
+    static int[] selected;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] split = br.readLine().split(" ");
+
+        N = Integer.parseInt(split[0]);
+        M = Integer.parseInt(split[1]);
+
+        selected = new int[M];
+        visited = new boolean[N];
+        int[] arr = new int[N];
+
+        split = br.readLine().split(" ");
+
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(split[i]);
+        }
+
+        Arrays.sort(arr);
+
+        backTracking(0, arr);
+
+        System.out.print(sb.toString());
+    }
+
+    public static void backTracking(int pick, int[] arr) {
+        if (pick == M) {
+            for(int i=0; i<selected.length; i++) {
+                sb.append(arr[selected[i]] + " ");
+            }
+
+            sb.append("\n");
+            
+            return;
+        }
+
+        for(int i=0; i<N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                selected[pick] = i;
+                backTracking(pick + 1, arr);
+                visited[i] = false;
+            }
+        }
+    }
+}
