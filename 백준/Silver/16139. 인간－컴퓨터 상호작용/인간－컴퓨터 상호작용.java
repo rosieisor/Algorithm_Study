e14529973a1d42dff149e437296f46b8dccb2f24
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-10-05 01:17:19 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 164 ms, Memory: 14144 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+
+        int q = Integer.parseInt(br.readLine());
+
+        Map<Character, int[]> map = new HashMap<>();
+
+        for(int i=0; i<26; i++) {
+            map.put((char)('a' + i), new int[S.length()]);
+        }
+
+        for(int i=0; i<26; i++) {
+            int sum = 0;
+            int[] arr = map.get((char)('a' + i));
+            
+            for(int j=0; j<S.length(); j++) {
+                if (S.charAt(j) == (char)('a' + i)) {
+                    sum += 1;    
+                }
+                arr[j] = sum;
+            }
+        }
+
+        for(int i=0; i<q; i++) {
+            String[] str = br.readLine().split(" ");
+
+            char a = str[0].charAt(0);
+            int start = Integer.parseInt(str[1]);
+            int end = Integer.parseInt(str[2]);
+
+            int[] arr = map.get(a);
+
+            if (start > 0) {
+                System.out.println(arr[end] - arr[start-1]);
+            } else {
+                System.out.println(arr[end]);
+            }
+        }
+    }
+}
