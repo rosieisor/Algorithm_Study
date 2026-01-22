73ac01c0c11bf0dacc0aa46fd153f6e24943972d
rosie98 <106025529+rosieisor@users.noreply.github.com>
2026-01-23 01:58:23 +0900

    [Gold II] Title: 문제집, Time: 528 ms, Memory: 66084 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+        
+        int N = Integer.parseInt(str[0]);
+        int M = Integer.parseInt(str[1]);
+
+        int[] input = new int[N + 1];   // 진입 차수
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i=0; i<=N; i++) {
+            list.add(new ArrayList<>());
+        }
+        
+        for(int i=0; i<M; i++) {
+            str = br.readLine().split(" ");
+
+            int A = Integer.parseInt(str[0]);
+            int B = Integer.parseInt(str[1]);
+            list.get(A).add(B);
+
+            input[B]++; 
+        }
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for(int i=1; i<=N; i++) {
+            if (input[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            result.add(node);
+
+            for(int n : list.get(node)) {
+                input[n]--;
+
+                if (input[n] == 0) {
+                    queue.offer(n);
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<N; i++) {
+            sb.append(result.get(i) + " ");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
